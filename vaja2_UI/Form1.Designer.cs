547865d47b4e411c76dd06352b3855c174425501
andrazv09 <andraz.vrecko@gmail.com>
2020-11-28 19:59:24 +0100

    first commit

---
+++ vaja2_UI/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace vaja2_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(296, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Difficulty:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 98);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(425, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 98);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(177, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 98);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(301, 179);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 98);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(425, 179);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 98);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(177, 283);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 98);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(301, 283);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 98);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(425, 283);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 98);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 98);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(326, 398);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "start";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ vaja2_UI/Form1.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace vaja2_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Serializable]
+        public struct algM
+        {
+            public int ocena;
+            public int[,] poteza;
+            public algM(int x, int[,]y)
+            {
+                ocena = x;
+                poteza = y;
+                
+            }
+        }
+
+        static public T DeepCopy<T>(T obj)
+        {
+            BinaryFormatter s = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                s.Serialize(ms, obj);
+                ms.Position = 0;
+                T t = (T)s.Deserialize(ms);
+
+                return t;
+            }
+        }
+
+        public int[,] gamePolje = new int[3, 3] {{0,0,0}, { 0, 0, 0 }, { 0, 0, 0}};
+        public int turn = 0; //1 - MAX, 2 - MIN
+        public int diff;
+
+        
+
+        public void printPolje(int[,] polje)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write(polje[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int hevOcena(int[,] polje)
+        {
+            int ocena = 0;
+
+
+                if ((polje[0, 0] == 1) && (polje[0, 1] == 1) && (polje[0, 2] == 1))
+                    return 1000;
+                if ((polje[1, 0] == 1) && (polje[1, 1] == 1) && (polje[1, 2] == 1))
+                    return 1000;
+                if ((polje[2, 0] == 1) && (polje[2, 1] == 1) && (polje[2, 2] == 1))
+                    return 1000;
+
+                if ((polje[0, 0] == 1) && (polje[1, 0] == 1) && (polje[2, 0] == 1))
+                    return 1000;
+                if ((polje[0, 1] == 1) && (polje[1, 1] == 1) && (polje[2, 1] == 1))
+                    return 1000;
+                if ((polje[0, 2] == 1) && (polje[1, 2] == 1) && (polje[2, 2] == 1))
+                    return 1000;
+
+                if ((polje[0, 0] == 1) && (polje[1, 1] == 1) && (polje[2, 2] == 1))
+                    return 1000;
+                if ((polje[0, 2] == 1) && (polje[1, 1] == 1) && (polje[2, 0] == 1))
+                    return 1000;
+                //winning pos MIN
+                if ((polje[0, 0] == 2) && (polje[0, 1] == 2) && (polje[0, 2] == 2))
+                    return -1000;
+                if ((polje[1, 0] == 2) && (polje[1, 1] == 2) && (polje[1, 2] == 2))
+                    return -1000;
+                if ((polje[2, 0] == 2) && (polje[2, 1] == 2) && (polje[2, 2] == 2))
+                    return -1000;
+
+                if ((polje[0, 0] == 2) && (polje[1, 0] == 2) && (polje[2, 0] == 2))
+                    return -1000;
+                if ((polje[0, 1] == 2) && (polje[1, 1] == 2) && (polje[2, 1] == 2))
+                    return -1000;
+                if ((polje[0, 2] == 2) && (polje[1, 2] == 2) && (polje[2, 2] == 2))
+                   return -1000;
+
+                if ((polje[0, 0] == 2) && (polje[1, 1] == 2) && (polje[2, 2] == 2))
+                    return -1000;
+                if ((polje[0, 2] == 2) && (polje[1, 1] == 2) && (polje[2, 0] == 2))
+                    return -1000;
+
+
+                //MAX
+                if ((polje[0, 0] == 0 || polje[0, 0] == 1) && (polje[0, 1] == 0 || polje[0, 1] == 1) && (polje[0, 2] == 0 || polje[0, 2] == 1))
+                    ocena++;
+                if ((polje[1, 0] == 0 || polje[1, 0] == 1) && (polje[1, 1] == 0 || polje[1, 1] == 1) && (polje[1, 2] == 0 || polje[1, 2] == 1))
+                    ocena++;
+                if ((polje[2, 0] == 0 || polje[2, 0] == 1) && (polje[2, 1] == 0 || polje[2, 1] == 1) && (polje[2, 2] == 0 || polje[2, 2] == 1))
+                    ocena++;
+
+                if ((polje[0, 0] == 0 || polje[0, 0] == 1) && (polje[1, 0] == 0 || polje[1, 0] == 1) && (polje[2, 0] == 0 || polje[2, 0] == 1))
+                    ocena++;
+                if ((polje[0, 1] == 0 || polje[0, 1] == 1) && (polje[1, 1] == 0 || polje[1, 1] == 1) && (polje[2, 1] == 0 || polje[2, 1] == 1))
+                    ocena++;
+                if ((polje[0, 2] == 0 || polje[0, 2] == 1) && (polje[1, 2] == 0 || polje[1, 2] == 1) && (polje[2, 2] == 0 || polje[2, 2] == 1))
+                    ocena++;
+
+                if ((polje[0, 0] == 0 || polje[0, 0] == 1) && (polje[1, 1] == 0 || polje[1, 1] == 1) && (polje[2, 2] == 0 || polje[2, 2] == 1))
+                    ocena++;
+                if ((polje[0, 2] == 0 || polje[0, 2] == 1) && (polje[1, 1] == 0 || polje[1, 1] == 1) && (polje[2, 0] == 0 || polje[2, 0] == 1))
+                    ocena++;
+                //MIN
+                if ((polje[0, 0] == 0 || polje[0, 0] == 2) && (polje[0, 1] == 0 || polje[0, 1] == 2) && (polje[0, 2] == 0 || polje[0, 2] == 2))
+                    ocena--;
+                if ((polje[1, 0] == 0 || polje[1, 0] == 2) && (polje[1, 1] == 0 || polje[1, 1] == 2) && (polje[1, 2] == 0 || polje[1, 2] == 2))
+                    ocena--;
+                if ((polje[2, 0] == 0 || polje[2, 0] == 2) && (polje[2, 1] == 0 || polje[2, 1] == 2) && (polje[2, 2] == 0 || polje[2, 2] == 2))
+                    ocena--;
+
+                if ((polje[0, 0] == 0 || polje[0, 0] == 2) && (polje[1, 0] == 0 || polje[1, 0] == 2) && (polje[2, 0] == 0 || polje[2, 0] == 2))
+                    ocena--;
+                if ((polje[0, 1] == 0 || polje[0, 1] == 2) && (polje[1, 1] == 0 || polje[1, 1] == 2) && (polje[2, 1] == 0 || polje[2, 1] == 2))
+                    ocena--;
+                if ((polje[0, 2] == 0 || polje[0, 2] == 2) && (polje[1, 2] == 0 || polje[1, 2] == 2) && (polje[2, 2] == 0 || polje[2, 2] == 2))
+                    ocena--;
+
+                if ((polje[0, 0] == 0 || polje[0, 0] == 2) && (polje[1, 1] == 0 || polje[1, 1] == 2) && (polje[2, 2] == 0 || polje[2, 2] == 2))
+                    ocena--;
+                if ((polje[0, 2] == 0 || polje[0, 2] == 2) && (polje[1, 1] == 0 || polje[1, 1] == 2) && (polje[2, 0] == 0 || polje[2, 0] == 2))
+                    ocena--;
+                return ocena;
+            
+            //winning pos MAX
+            
+        }
+
+        public bool gameOver(int[,] polje)//preveri ce je list
+        {
+            if (hevOcena(polje) == 1000 || hevOcena(polje) == -1000)
+                return true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (polje[i, j] == 0)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public algM minmaks(int[,] polje, int poteza, int d)
+        {
+            algM bestcase = new algM();
+            if (gameOver(polje) || d == 0)
+            {
+                algM temp = new algM(hevOcena(polje), null);
+                return temp;
+            }
+                
+            int ocena;
+            if (poteza == 1) //MAX
+            {
+                ocena = -1000;
+            }
+            else
+            {//MIN
+                ocena = 1000;
+            }
+            algM retVal = new algM();
+
+            //bestcase.poteza = DeepCopy(polje);
+            //bestcase.poteza = polje;
+
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if(polje[i,j] == 0)
+                    {
+                        bestcase.poteza = DeepCopy(polje); //gre do prve 0, to 0 nastavi na stevilko igralca
+                        bestcase.poteza[i, j] = DeepCopy(poteza);
+
+                        if (poteza == 1)
+                            poteza = 2;
+                        else
+                            poteza = 1;
+
+                        algM temp2 = minmaks(bestcase.poteza, poteza, d - 1);
+
+                        if (poteza == 1)
+                            poteza = 2;
+                        else
+                            poteza = 1;
+
+                        if ((poteza == 1 && temp2.ocena > ocena) || (poteza == 2 && temp2.ocena < ocena)) {
+                            ocena = temp2.ocena;
+                            retVal.ocena = temp2.ocena;
+                            retVal.poteza = DeepCopy(bestcase.poteza);
+                        }
+                    }
+                }
+            }
+
+            //retVal.ocena = ocena;
+            return retVal;
+        }
+
+        public void colorBoard()
+        {
+            if (gamePolje[0, 0] == 1)
+                button1.BackColor = Color.Blue;
+            if (gamePolje[1, 0] == 1)
+                button4.BackColor = Color.Blue;
+            if (gamePolje[2, 0] == 1)
+                button7.BackColor = Color.Blue;
+            if (gamePolje[0, 1] == 1)
+                button2.BackColor = Color.Blue;
+            if (gamePolje[0, 2] == 1)
+                button3.BackColor = Color.Blue;
+            if (gamePolje[1, 1] == 1)
+                button5.BackColor = Color.Blue;
+            if (gamePolje[1, 2] == 1)
+                button6.BackColor = Color.Blue;
+            if (gamePolje[2, 1] == 1)
+                button8.BackColor = Color.Blue;
+            if (gamePolje[2, 2] == 1)
+                button9.BackColor = Color.Blue;
+
+            if (gamePolje[0, 0] == 2)
+                button1.BackColor = Color.Red;
+            if (gamePolje[1, 0] == 2)
+                button4.BackColor = Color.Red;
+            if (gamePolje[2, 0] == 2)
+                button7.BackColor = Color.Red;
+            if (gamePolje[0, 1] == 2)
+                button2.BackColor = Color.Red;
+            if (gamePolje[0, 2] == 2)
+                button3.BackColor = Color.Red;
+            if (gamePolje[1, 1] == 2)
+                button5.BackColor = Color.Red;
+            if (gamePolje[1, 2] == 2)
+                button6.BackColor = Color.Red;
+            if (gamePolje[2, 1] == 2)
+                button8.BackColor = Color.Red;
+            if (gamePolje[2, 2] == 2)
+                button9.BackColor = Color.Red;
+
+            if (gamePolje[0, 0] == 0)
+                button1.BackColor = Color.LightGray;
+            if (gamePolje[1, 0] == 0)
+                button4.BackColor = Color.LightGray;
+            if (gamePolje[2, 0] == 0)
+                button7.BackColor = Color.LightGray;
+            if (gamePolje[0, 1] == 0)
+                button2.BackColor = Color.LightGray;
+            if (gamePolje[0, 2] == 0)
+                button3.BackColor = Color.LightGray;
+            if (gamePolje[1, 1] == 0)
+                button5.BackColor = Color.LightGray;
+            if (gamePolje[1, 2] == 0)
+                button6.BackColor = Color.LightGray;
+            if (gamePolje[2, 1] == 0)
+                button8.BackColor = Color.LightGray;
+            if (gamePolje[2, 2] == 0)
+                button9.BackColor = Color.LightGray;
+
+        }
+        public void gameStart()
+        {
+            turn = 1;
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    gamePolje[i, j] = 0;
+                }
+            }
+            colorBoard();
+            diff = int.Parse(comboBox1.SelectedItem.ToString());
+            Console.WriteLine(diff);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(turn == 1)
+            {
+                gamePolje[0, 0] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[0, 1] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //int[,] polje = new int[3, 3] { { 0, 0, 2 }, { 0, 1, 1}, { 0, 0, 0 } };
+            //Console.WriteLine(hevOcena(polje));
+            //algM a = minmaks(polje, 2, 2);
+            //printPolje(a.poteza);
+            gameStart();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[0, 2] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[1, 0] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[1, 1] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[1, 2] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[2, 0] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[2, 1] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (turn == 1)
+            {
+                gamePolje[2, 2] = 1;
+                colorBoard();
+                if (!gameOver(gamePolje))
+                {
+                    algM temp = minmaks(gamePolje, 2, diff);
+                    gamePolje = DeepCopy(temp.poteza);
+                }
+                else
+                {
+                    turn = 0;
+                }
+                colorBoard();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
